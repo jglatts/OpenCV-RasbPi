f3afcc80ae55baa5f572c73ee698cc6360e859e1
John Glatts <john@z-axiscc.com>
2021-06-15 08:29:14 -0400

    Update detect.py

--- OpenCV-RasbPi/detect.py
+++ OpenCV-RasbPi/detect.py
@@ -1,6 +1,10 @@
 """
     Simple OpenCV-Python script for face detection
     author: JDG
+
+    ToDo:
+        - implement eye detection
+
 """
 import cv2
 
@@ -13,11 +17,12 @@ def detect_face():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
         for (x, y, w, h) in faces:
+            print("Drawing a rectangle!\n")
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        img = cv2.resize(img, (250, 250))
         cv2.imshow('Face Detect', img)
         if  cv2.waitKey(1) & 0xff == ord('q'): break
     cam.release()
-
 
 if __name__ == "__main__":
     detect_face()
