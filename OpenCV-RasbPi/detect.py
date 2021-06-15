8954d05774f41f03a726276e8f60b3542e39e473
John Glatts <john@z-axiscc.com>
2021-06-15 16:35:57 -0400

    Update detect.py

--- OpenCV-RasbPi/detect.py
+++ OpenCV-RasbPi/detect.py
@@ -7,22 +7,27 @@
 
 """
 import cv2
+import imutils
 
-def detect_face():
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    cam = cv2.VideoCapture(0)
-    while True:
-        ret, img = cam.read()
-        if ret is None: break 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-        for (x, y, w, h) in faces:
-            print("Drawing a rectangle!\n")
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        img = cv2.resize(img, (250, 250))
-        cv2.imshow('Face Detect', img)
-        if  cv2.waitKey(1) & 0xff == ord('q'): break
-    cam.release()
+class Detector():
+    def detect_face():
+        face_count = 0
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        cam = cv2.VideoCapture(0)
+        while True:
+            ret, img = cam.read()
+            if ret is None: break 
+            img = imutils.rotate_bound(img, 180)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            for (x, y, w, h) in faces:
+                face_count += 1
+                print("Detected Faces: {}\n".format(face_count))
+                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            img = cv2.resize(img, (300, 300))
+            cv2.imshow('Face Detect', img)
+            if  cv2.waitKey(1) & 0xff == ord('q'): break
+        cam.release()
 
 if __name__ == "__main__":
-    detect_face()
+    Detector.detect_face()
